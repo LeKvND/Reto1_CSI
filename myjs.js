ba88e36c8aff7d52332a2be7711c39219b583c3a
LeKvND <45203956+LeKvND@users.noreply.github.com>
2021-03-03 02:28:46 -0600

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,21 @@
+function openTab(tabName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+	tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+	tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(tabName).style.display = "block";
+    if(elmnt.style != null) {
+	elmnt.style.backgroundColor = color;
+    }
+}
+
+window.onload = function() {
+    openTab('tabintro', this, '#222');
+}
+
+/* En JS hay funciones que pueden ser utiles. Aqui esta la parte 3/3 de la bandera: _Ut1L_73bvf882j} */
